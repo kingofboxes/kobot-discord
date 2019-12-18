7cf66d41e26e5f235bfcab2476b219e36b2a10c5
kingofboxes <jliang1641@gmail.com>
2019-12-18 15:34:35 +1100

    Reformatted the reminder feature so that it accepts more than just minutes

--- modules/reminder.py
+++ modules/reminder.py
@@ -1,6 +1,9 @@
 import discord
+import re
+from .utilities.logger import *
 from discord.ext import tasks, commands
 from datetime import datetime, timedelta
+from dateutil.relativedelta import *
 
 # Dice module.
 class Reminders(commands.Cog):
@@ -17,7 +20,9 @@ class Reminders(commands.Cog):
         # !remindme <duration>
         # !remindme <duration> <message>
         input = ctx.message.content.split(' ', 2)
-        summary = "No reminder description specified."
+        summary = "(no description)"
+        curr_time = datetime.now()
+        curr_time_s = curr_time.strftime("%d/%m/%Y at %H:%M:%S")
         valid = False
 
         # Put together the reminder.
@@ -25,33 +30,32 @@ class Reminders(commands.Cog):
             pass
 
         elif len(input) == 2:
-            if input[1].lstrip("-").isdigit():
-                duration = input[1]
-                if int(duration) > 0: valid = True
-                reminder = f"Reminding you for something you've set {input[1]} minutes ago."
+            duration = self.generateDuration(input[1])
+            if duration is not None:
+                valid = True
 
         else:
-            if input[1].lstrip("-").isdigit():
-                duration = input[1]
+            duration = self.generateDuration(input[1])
+            if duration is not None:
+                valid = True
                 summary = input[2]
-                if int(duration) > 0: valid = True
-                reminder = f"Reminder from {input[1]} minutes ago: {input[2]}."
 
         # Send the reminder or usage information.
         if valid:
             
             # Create a dictionary.
-            d_reminder = {'id' : ctx.message.author.id,
-                    'reminder' : reminder,
-                    'summary' : summary,
-                    'time' : datetime.now() + timedelta(minutes=int(duration))}
+            d_reminder = {  'id' : ctx.message.author.id,
+                            'reminder' : f"Your reminder from {curr_time_s}: {summary}",
+                            'summary' : summary,
+                            'time' : curr_time + relativedelta(years=+duration[0], months=+duration[1], days=+duration[2], hours=+duration[3], minutes=+duration[4])}
 
             # Append the dictionary to the list of reminders and sorts it.
-            message = f"Reminder set. Reminding you in {duration} minutes."
+            reminder_time = d_reminder['time'].strftime("%d/%m/%Y at %H:%M:%S")
+            message = f"Reminder set for {reminder_time}."
             self._reminders.append(d_reminder)
             
         else:
-            message = "```Usage: !remindme <time> [description]; time >= 0```"
+            message = "```Usage: !remindme <time> [description]\ntime = #Y#M#d#h#m (e.g. 1Y1d1h1m = 1 year, 0 months, 1 day, 1 hour, 1 minute)```"
 
         await ctx.message.channel.send(message)
              
@@ -60,7 +64,7 @@ class Reminders(commands.Cog):
     async def check_reminders(self):
         
         # Sort the reminders everytime you check.
-        print(self._reminders)
+        # print(self._reminders)
         self._reminders = sorted(self._reminders, key = lambda i : i['time'])
 
         # Keep track of the removed entries.
@@ -87,3 +91,27 @@ class Reminders(commands.Cog):
     # Getter for reminders list.
     def getRemindersList(self):
         return self._reminders
+
+    # Uses regex to get the fields to make a new timedelta object.
+    # Group 1 = Year, Group 2 = Month, 
+    # Group 3 = Day, Group 4 = Hour, 
+    # Group 5 = Minute
+    def generateDuration(self, dateStr):
+        
+        # Constants
+        MINUTES_IN_YEAR = 525600
+        MINUTES_IN_MONTH = 43800
+        MINUTES_IN_DAY = 1440
+        MINUTES_IN_HOUR = 60
+
+        dateFmt = r"^\s*(?:(\d+)Y)?" + \
+                r"\s*(?:(\d+)M)?" + \
+                r"\s*(?:(\d+)d)?" + \
+                r"\s*(?:(\d+)h)?" + \
+                r"\s*(?:(\d+)m)?\s*$"
+
+        match = re.search(dateFmt, dateStr)
+        if match is not None:
+            duration = [int(x) for x in match.groups(default="0")]
+            return duration
+
