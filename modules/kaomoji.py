8740fe3fe27edbabcd61a4bd6f7159bd1e91d705
Justin Liang <justin.liang1@unsw.edu.au>
2020-05-17 13:25:06 +1000

    Finished Kaomoji cog

--- modules/kaomoji.py
+++ modules/kaomoji.py
@@ -1,17 +1,13 @@
 import discord
-import re
 import json
 import glob
-from .utilities.logger import *
 from discord.ext import tasks, commands
-from datetime import datetime
-
 
 class Kaomoji(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.kDict = self.initKaomojiCog()
+        self._kDict = self.initKaomojiCog()
 
     # Load kaomojis.
     def initKaomojiCog(self):
@@ -31,12 +27,44 @@ class Kaomoji(commands.Cog):
         for k in kaomojis:
             kDict = {**kDict, **k}
 
-        print(kDict)
         return kDict
+
+    # Getter for the dictionary.
+    def getKDict(self):
+        return self._kDict
     
     # Either gets you a list of kaomojis or gives you a list of categories for kaomojis.
     @commands.command(help='Lists kaomojis available')
     async def klist(self, ctx):
 
-        await ctx.message.author.send("To be implemented.")
-        await ctx.message.channel.send("A list of available kaomojis have been sent to your DMs.")
+        # Use cases:
+        # !klist
+        # !klist <category>
+        input = ctx.message.content.split(' ', 2)
+        kDict = self.getKDict()
+        categories = list(kDict.keys())
+
+        # Only when klist itself is invoked.
+        if len(input) == 1 or len(input) > 2:
+            message = f"```Usage: !klist [category] (e.g. !klist joy)\nCurrent available categories are: {categories}```"
+        
+        # If only two arguments are specified.
+        else:
+            cat_arg = input[1]
+            if cat_arg in categories:
+                kaomoji = kDict[cat_arg]
+                category = cat_arg.title()
+                message = f"```{category}:\n"
+                for k in kaomoji:
+                    message = message + f"*\t{k}" + "\n"
+                message = message + "```"
+
+            else:
+                message = f"Invalid category, please try again."
+
+        # Check if message was sent in server or DM.
+        if ctx.guild is not None:
+            await ctx.message.channel.send("A list of available kaomojis have been sent to your DMs.")
+        
+        # Send the correct response to the person's DM.
+        await ctx.message.author.send(message)
